cb66771a2751fd24e03ed77c9998ca078e626242
snopflake <nofanisrina@gmail.com>
2025-06-23 09:53:22 +0700

    create fill animation

---
+++ lib/widgets/fill_animation.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class FillAnimation extends StatefulWidget {
+  final bool shouldFill;
+  final Widget child;
+  final Duration duration;
+  final Color fillColor;
+
+  const FillAnimation({
+    super.key,
+    required this.shouldFill,
+    required this.child,
+    this.duration = const Duration(milliseconds: 800),
+    this.fillColor = Colors.orange,
+  });
+
+  @override
+  State<FillAnimation> createState() => _FillAnimationState();
+}
+
+class _FillAnimationState extends State<FillAnimation>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant FillAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.shouldFill && !oldWidget.shouldFill) {
+      _controller.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        widget.child, 
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return ClipRect(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                widthFactor: _animation.value,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: widget.fillColor.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: widget.child, 
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
