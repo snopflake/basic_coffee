ec5cca9061fc2b422e712aa06e972e8f1cd6c9ee
snopflake <nofanisrina@gmail.com>
2025-06-23 09:52:14 +0700

    create shake animation

--- lib/widgets/coffee_card.dart
+++ lib/widgets/coffee_card.dart
@@ -1,46 +1,90 @@
 import 'package:flutter/material.dart';
 import '../models/coffee.dart';
 
-class CoffeeCard extends StatelessWidget {
+class CoffeeCard extends StatefulWidget {
   final Coffee coffee;
   final bool affordable;
-  final VoidCallback onTap;
+  final VoidCallback onSuccess;
+  final VoidCallback onFail;
 
   const CoffeeCard({
     super.key,
     required this.coffee,
     required this.affordable,
-    required this.onTap,
+    required this.onSuccess,
+    required this.onFail,
   });
+
+  @override
+  State<CoffeeCard> createState() => _CoffeeCardState();
+}
+
+class _CoffeeCardState extends State<CoffeeCard> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _shakeAnim;
+  bool _isFilled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _shakeAnim = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0.05, 0),
+    ).chain(CurveTween(curve: Curves.elasticIn)).animate(_controller);
+  }
+
+  void _onTap() {
+    if (widget.affordable) {
+      setState(() => _isFilled = true);
+      widget.onSuccess();
+    } else {
+      _controller.forward(from: 0);
+      widget.onFail();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 600),
-        curve: Curves.easeInOut,
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: affordable ? Colors.orange[300] : Colors.grey[200],
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                const Icon(Icons.local_cafe, size: 28),
-                const SizedBox(width: 12),
-                Text(
-                  coffee.name,
-                  style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-            Text('${coffee.price} Tokens'),
-          ],
+      onTap: _onTap,
+      child: SlideTransition(
+        position: _shakeAnim,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: _isFilled ? Colors.orange : Colors.grey[200],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Icon(Icons.local_cafe, size: 28),
+                  const SizedBox(width: 12),
+                  Text(
+                    widget.coffee.name,
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              Text('${widget.coffee.price} Tokens'),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/shake_animation_wrapper.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ShakeAnimationWrapper extends StatefulWidget {
+  final Widget child;
+  final bool shouldShake;
+
+  const ShakeAnimationWrapper({
+    super.key,
+    required this.child,
+    required this.shouldShake,
+  });
+
+  @override
+  State<ShakeAnimationWrapper> createState() => _ShakeAnimationWrapperState();
+}
+
+class _ShakeAnimationWrapperState extends State<ShakeAnimationWrapper>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+
+    _offsetAnimation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: -8.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: -8.0, end: 8.0), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: 8.0, end: -8.0), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: -8.0, end: 8.0), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: 8.0, end: 0.0), weight: 1),
+    ]).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+  }
+
+  @override
+  void didUpdateWidget(covariant ShakeAnimationWrapper oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.shouldShake && !oldWidget.shouldShake) {
+      _controller.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _offsetAnimation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(_offsetAnimation.value, 0),
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
