9d8a5d6b3a05385d56a0c3d3586688707656984d
snopflake <nofanisrina@gmail.com>
2025-06-23 09:44:14 +0700

    create coffee data (dummy)

---
+++ lib/data/coffee_data.dart
@@ -0,0 +1,8 @@
+import '../models/coffee.dart';
+
+final List<Coffee> coffeeList = [
+  Coffee(id: 1, name: "Classic Latte", price: 250),
+  Coffee(id: 2, name: "Americano", price: 250),
+  Coffee(id: 3, name: "Frappuccino", price: 600),
+  Coffee(id: 4, name: "Milk Tea Boba", price: 700),
+];
